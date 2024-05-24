e9d20afac28278be73fe89e14136b2815b22b4c9
kabraliev2005 <kabraliev2005@gmail.com>
2024-05-24 07:52:28 +0500

    fix

--- moderator/views.py
+++ moderator/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import generics
 from . import models
 from . import serializer
@@ -20,6 +21,20 @@ class ModeratorProjectsRetriveView(generics.RetrieveAPIView):
     queryset = models.Moderator.objects.all()
     serializer_class = serializer.ProjectsRetriveSerializer
 
-    def get(self, request, *args, **kwargs):
-        self.queryset = self.queryset.get(projects__id=kwargs['project_pk']).projects
-        return super().get(request, *args, **kwargs)
+
+    def get_object(self):
+        moderator_pk = self.kwargs['pk']
+        project_pk = self.kwargs['project_pk']
+        
+        # Ensure the project is associated with the given moderator
+        try:
+            moderator = models.Moderator.objects.get(pk=moderator_pk, projects__id=project_pk)
+            project = moderator.projects.get(pk=project_pk)
+        except models.Moderator.DoesNotExist:
+            raise Http404("Moderator or Project not found")
+        
+        return project
+
+    # def get(self, request, *args, **kwargs):
+    #     self.queryset.get(projects__id=kwargs['project_pk']).projects
+    #     return super().get(request, *args, **kwargs)
